3828491fa6ea5828d2d0168b9415f15995229563
gopal-mac <gopal@appnings.com>
2020-02-21 14:13:44 +0530

    first commit

---
+++ src/utils/location.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const location = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/99af5e7654f4c9066993bb0636d37403/'+ latitude + ',' + longitude
+
+    request( { url, json: true }, (error, {body}) => {
+        if(error){
+            callback('Unable to connect to weather services!', undefined)
+        }else if(body.error){
+            console.log('Unable to find location', undefined)
+        }else{
+            const data = body;
+            callback(undefined, data.hourly.summary + 'It is currently ' + data.currently.temperature + ' degree out. There is a ' + data.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = location
